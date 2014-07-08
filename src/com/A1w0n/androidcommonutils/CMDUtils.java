916e0f731adcb551b12bc2740290fcd66acb799a
William <faip521@gmail.com>
2014-07-08 15:33:18 +0800

    1.增加CMDUtils和截屏的封装

---
+++ src/com/A1w0n/androidcommonutils/CMDUtils.java
@@ -0,0 +1,79 @@
+package com.A1w0n.androidcommonutils;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+
+import com.A1w0n.androidcommonutils.IOUtils.IOUtils;
+import com.A1w0n.androidcommonutils.debugutils.Logger;
+
+public class CMDUtils {
+
+	private CMDUtils() {
+	}
+
+	public static boolean runWithRoot(String command) {
+		int result = -1;
+		
+		Process process = null;
+		DataOutputStream os = null;
+		InputStream is = null;
+		InputStream es = null;
+		try {
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(command + "\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			result = process.waitFor();
+			Logger.d("Run command : " +command + ", process return value : " + result);
+			is = process.getInputStream();
+			es = process.getErrorStream();
+			Logger.d(IOUtils.toString(process.getInputStream()));
+			Logger.e(IOUtils.toString(process.getErrorStream()));
+		} catch (Exception e) {
+			return false;
+		} finally {
+			IOUtils.closeSilently(os);
+			IOUtils.closeSilently(is);
+			IOUtils.closeSilently(es);
+			if (process != null) {
+				process.destroy();
+			}
+		}
+		
+		return result == 1;
+	}
+	
+	public static boolean runWithoutRoot(String command) {
+		int result = -1;
+		
+		Process process = null;
+		DataOutputStream os = null;
+		InputStream is = null;
+		InputStream es = null;
+		try {
+			process = Runtime.getRuntime().exec("sh");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(command + "\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			result = process.waitFor();
+			Logger.d("Run command : " +command + ", process return value : " + result);
+			is = process.getInputStream();
+			es = process.getErrorStream();
+			Logger.d(IOUtils.toString(process.getInputStream()));
+			Logger.e(IOUtils.toString(process.getErrorStream()));
+		} catch (Exception e) {
+			return false;
+		} finally {
+			IOUtils.closeSilently(os);
+			IOUtils.closeSilently(is);
+			IOUtils.closeSilently(es);
+			if (process != null) {
+				process.destroy();
+			}
+		}
+		
+		return result == 1;
+	}
+}
